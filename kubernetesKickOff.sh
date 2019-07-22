7ebe34b12877998008989970e40e2dd545e35d26
nboaram <nboaram1@hotmail.co.uk>
2019-07-22 14:24:22 +0100

    basic scripts ready to be overwritten as needed

---
+++ kubernetesKickOff.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+kubectl apply -f number1
+kubectl apply -f number2
+kubectl apply -f number3
+kubectl apply -f number4
+kubectl apply -f number5
---
+++ kubernetesTearDown.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+kubectl delete -f number1
+kubectl delete -f number2
+kubectl delete -f number3
+kubectl delete -f number4
+kubectl delete -f number5
